a91189be52081f44f3ade77a595b3d8cf533c5c0
jez23 <jblackmoreuk@hotmail.co.uk>
2017-09-04 20:47:45 +0100

    Added Google API

--- app/components/StoreLocator.jsx
+++ app/components/StoreLocator.jsx
@@ -1,14 +1,16 @@
 var React = require('react')
+var Map = require('./Map')
 var StoreLocator = React.createClass({
     render: function() {
         return (
             <div className='store-locator'>
-                    <div className='location-column'>
-                        <div className='title'>
-                            Store Locator
-                        </div>
+                <div className='location-column'>
+                    <div className='title'>
+                        Store Locator
                     </div>
+                </div>
                 <div className='map'>
+                    <Map lat="53.4719986" lng="-2.2414979"/>
                 </div>
             </div>
         )
